be92ff22a5c3f9f032345f678ac2ddb7375ecdf9
NileshRamcharan <58693891+NileshRamcharan@users.noreply.github.com>
2022-03-26 15:32:31 +0100

    npm fix to run npm start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+ return(
+   <Router>
+     <div className="App">
+      <Routes>
+        <Route path="/" element ={<Start></Start>} />
+      </Routes>
+     </div>
+  </Router>
+ )
 }
 
 export default App;
