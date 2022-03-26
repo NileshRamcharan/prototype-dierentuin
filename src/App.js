819f5228f732e9fbbf7c44e151b760f3c2d3133a
NileshRamcharan <58693891+NileshRamcharan@users.noreply.github.com>
2022-03-26 18:00:13 +0100

    puzzleLock done
    combinatie slot af (zonder styling)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-//import DragAndDrop from './components/DragAndDrop';
 import Start from './components/gameplay/Start';
 import OvergangPuzzelEen from './components/gameplay/OvergangPuzzelEen';
+import DragAndDrop from './components/puzzels/DragAndDrop';
+
+import OplosmatrixLock from './components/oplosmatrix/oplosmatrixLock';
+
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Start></Start>} />
-          <Route path='/firstPuzzle' element={<OvergangPuzzelEen />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+ return(
+   <Router>
+     <div className="App">
+      <Routes>
+        <Route path="/" element ={<Start></Start>} />
+        <Route path='/firstPuzzle' element={<OvergangPuzzelEen />} />
+        <Route path='/puzzleMap' element={ <DragAndDrop />} />
+        <Route path= '/oplosmatrix' element ={<OplosmatrixLock />} />
+      </Routes>
+     </div>
+  </Router>
+ )
 }
 
 export default App;
